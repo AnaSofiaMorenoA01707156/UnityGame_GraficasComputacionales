2b5edc5c9d82bd9a73e04dcbe892bed2c772158f
AnaSofiaMorenoA01707156 <A01707156@tec.mx>
2026-08-30 15:58:25 -0600

    [Feat] Añadir sonido de golpe al golpear cualquier clavo.

--- GameGrayBox/Assets/Scripts/NailScript.cs
+++ GameGrayBox/Assets/Scripts/NailScript.cs
@@ -5,6 +5,11 @@ public class NailScript : MonoBehaviour
     private bool golpeado = false;
     public float clavadoAbajo = -0.2f;
     public ParticleSystem particulas; //obtener objeto con sistema de partículas vinculado a cada clavo
+    private AudioSource golpe;
+    void Start()
+    {
+        golpe = GetComponent<AudioSource>(); //obtener componente propio de audiosource con el sonido de golpe
+    }
     void OnTriggerEnter(Collider other)
     {
         //si choca con el martillo
@@ -16,6 +21,7 @@ public class NailScript : MonoBehaviour
                 {
                     golpeado = true; //ya fue golpeado
                     particulas.Play(); //ejecutar sistema de partículas al momento de impacto del clavo
+                    golpe.Play();
                     transform.position = transform.position + new Vector3(0f, clavadoAbajo, 0f); //se baja para mostrarse clavado
                     ManagerScript manager = Object.FindFirstObjectByType<ManagerScript>();
                     if (manager != null) manager.Clavar();
